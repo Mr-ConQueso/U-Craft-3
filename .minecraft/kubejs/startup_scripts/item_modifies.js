ad7753d4295461cbed1adff9c879ffce0d4a0e1f
Mr. ConQueso <78820389+Mr-ConQueso@users.noreply.github.com>
2024-09-06 02:11:33 +0200

    beta 8.1

---
+++ .minecraft/kubejs/startup_scripts/item_modifies.js
@@ -0,0 +1,63 @@
+// priority: 0
+
+ItemEvents.modification(event => {
+    event.modify('minecraft:ender_pearl', item => {
+      item.maxStackSize = 48
+    })
+    event.modify('minecraft:bucket', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('exposure:color_film', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('exposure:black_and_white_film', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('minecraft:snowball', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('minecraft:egg', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('supplementaries:bomb_blue', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('supplementaries:bomb', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('etcetera:eggple', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('etcetera:golden_eggple', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('galosphere:silver_bomb', item => {
+        item.maxStackSize = 48
+    })
+    event.modify('minecraft:cake', item => {
+        item.maxStackSize = 64
+    })
+    event.modify('minecraft:potion', item => {
+        item.maxStackSize = 16
+    })
+    event.modify('minecraft:splash_potion', item => {
+        item.maxStackSize = 16
+    })
+    event.modify('minecraft:lingering_potion', item => {
+        item.maxStackSize = 16
+    })
+    event.modify('minecraft:minecart', item => {
+        item.maxStackSize = 16
+    })
+
+
+    event.modify('waterballoon:balloon_bazooka', item => {
+        item.maxStackSize = 1
+    })
+    event.modify('waterballoon:slingshot', item => {
+        item.maxStackSize = 1
+    })
+
+  })
+
+console.info('Modified Kubje.js items')
---
+++ .minecraft/kubejs/startup_scripts/mod_names.js
@@ -0,0 +1,7 @@
+// priority: 0
+
+Platform.mods.kubejs.name = 'U-Craft 3'
+//Platform.mods.under_the_moon.name = 'Under The Moon'
+Platform.mods.undead_revamp2.name = 'The Undead Revamp'
+
+console.info('Modified Kubje.js mod names')
