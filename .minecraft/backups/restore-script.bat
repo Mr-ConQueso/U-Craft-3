571ccc16c6c65df87d504ea8ea7b800317c8ed5f
Mr. ConQueso <78820389+Mr-ConQueso@users.noreply.github.com>
2024-09-08 02:09:52 +0200

    beta 8.4

---
+++ .minecraft/backups/restore-script.bat
@@ -0,0 +1,5 @@
+@echo off 
+/:
+cd "/home/nacho/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/U-Craft 3 Optimized/.minecraft/mods"
+java -jar "AdvancedBackups-forge-1.20-3.6.3.jar"
+pause
---
+++ .minecraft/backups/restore-script.sh
@@ -0,0 +1,2 @@
+cd "/home/nacho/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/U-Craft 3 Optimized/.minecraft/mods"
+java -jar "AdvancedBackups-forge-1.20-3.6.3.jar"
