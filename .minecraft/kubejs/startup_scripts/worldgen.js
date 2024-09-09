5dd54fb4bf5cfdeab7a753d311e23a6959ff91ae
Mr. ConQueso <78820389+Mr-ConQueso@users.noreply.github.com>
2024-09-09 02:07:52 +0200

    beta 8.6

---
+++ .minecraft/kubejs/startup_scripts/worldgen.js
@@ -0,0 +1,13 @@
+// priority: 0
+/*
+WorldgenEvents.remove( event => {
+  
+    event.removeOres(props => {
+
+      props.blocks = ['undead_revamp2:bostroxore']
+      
+    })
+  })
+
+console.info('Loaded Kube.js worldgen edits)')
+*/
